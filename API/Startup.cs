be51afa638ea085e043684643dc5cb22b2de043e
stephen-talentpay <stephen@talentpay.com>
2020-04-05 10:35:05 +1000

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,9 +27,16 @@ namespace API
       services.AddControllers();
       services.AddDbContext<StoreContext>(opt =>
         opt.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-      
+
       services.AddApplicationServices();
       services.AddSwaggerDocumentation();
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+              {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+              });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +49,8 @@ namespace API
 
       app.UseRouting();
       app.UseStaticFiles();
+
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
